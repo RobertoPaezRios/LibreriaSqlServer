c5f198151dee626462bcaa455b6104542facca15
Roberto Paez <robepa2009@gmail.com>
2021-12-15 22:53:14 +0100

    BETA 1.0

---
+++ BibliotecaDB/BibliotecaDB/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace BibliotecaDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIsbn = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.Borrar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Editar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Movimiento = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnSend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Borrar,
+            this.Editar,
+            this.Movimiento});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(842, 358);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(15, 438);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(443, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 467);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Autor";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(15, 483);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(443, 20);
+            this.txtAutor.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 515);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ISBN";
+            // 
+            // txtIsbn
+            // 
+            this.txtIsbn.Location = new System.Drawing.Point(15, 531);
+            this.txtIsbn.Name = "txtIsbn";
+            this.txtIsbn.Size = new System.Drawing.Size(443, 20);
+            this.txtIsbn.TabIndex = 5;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 377);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "INSERT";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(96, 376);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(177, 376);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "REFRESH";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(258, 376);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Borrar
+            // 
+            this.Borrar.HeaderText = "Borrar";
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Text = "Borrar";
+            this.Borrar.ToolTipText = "Borrar";
+            this.Borrar.UseColumnTextForButtonValue = true;
+            // 
+            // Editar
+            // 
+            this.Editar.HeaderText = "Editar";
+            this.Editar.Name = "Editar";
+            this.Editar.Text = "Editar";
+            this.Editar.ToolTipText = "Editar";
+            this.Editar.UseColumnTextForButtonValue = true;
+            // 
+            // Movimiento
+            // 
+            this.Movimiento.HeaderText = "Movimiento";
+            this.Movimiento.Name = "Movimiento";
+            this.Movimiento.Text = "Movimiento";
+            this.Movimiento.ToolTipText = "Movimiento";
+            this.Movimiento.UseColumnTextForButtonValue = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(464, 438);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 11;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 566);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIsbn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIsbn;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridViewButtonColumn Borrar;
+        private System.Windows.Forms.DataGridViewButtonColumn Editar;
+        private System.Windows.Forms.DataGridViewButtonColumn Movimiento;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ BibliotecaDB/BibliotecaDB/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BibliotecaDB
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conexionDB = new SqlConnection("Data Source = localhost; Initial Catalog = biblioteca; Integrated Security = True");
+        string idEditar = "";
+        int cont = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conexionDB.Open();
+            SqlCommand comando = new SqlCommand("SELECT * FROM libros;", conexionDB);
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            adaptador.SelectCommand = comando;
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+        }
+
+        public bool comprobarISBN(string isbn)
+        {
+            int nFilas = dataGridView1.Rows.Count - 1;
+            bool repetido = false;
+
+            for (int i = 0; i < nFilas; i++)
+            {
+                DataGridViewRow fila = dataGridView1.Rows[i];
+                if (fila.Cells[6].Value.ToString() == isbn)
+                {
+                    repetido = true;
+                    break;
+                }
+            }
+
+            return repetido;
+        }
+
+        public bool todoNumeros(string s)
+        {
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string autor = txtAutor.Text;
+            string isbn = txtIsbn.Text;
+            
+            if (todoNumeros(isbn))
+            {
+                if (txtIsbn.Text.Length <= 13)
+                {
+                    if (!comprobarISBN(txtIsbn.Text))
+                    {
+                        if (nombre != "" && autor != "")
+                        {
+                            SqlCommand comando = new SqlCommand("INSERT INTO libros VALUES ('" + nombre + "', '" + autor + "', '" + isbn + "');", conexionDB);
+                            int r = comando.ExecuteNonQuery();
+                            SqlCommand comand = new SqlCommand("SELECT * FROM libros;", conexionDB);
+                            SqlDataAdapter adaptador = new SqlDataAdapter();
+                            adaptador.SelectCommand = comand;
+                            DataTable tabla = new DataTable();
+                            adaptador.Fill(tabla);
+                            dataGridView1.DataSource = tabla;
+                            txtNombre.Text = "";
+                            txtAutor.Text = "";
+                            txtIsbn.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Tienes que rellenar todos los campos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No puedes repetir el campo 'isbn'", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El campo 'isbn' puedes tener un longitud maxima de 13 numeros", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Solo puedes introducir elementos numéricos en el campo ISBN", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (todoNumeros(txtIsbn.Text))
+            {
+                if (txtNombre.Text != "" && txtAutor.Text != "" && txtIsbn.Text != "")
+                {
+                    string query = "DELETE FROM libros WHERE nombre = '" + txtNombre.Text + "' AND autor ='" + txtAutor.Text + "' AND isbn = '" + txtIsbn.Text + "'";
+                    SqlCommand comando = new SqlCommand(query, conexionDB);
+                    int r = comando.ExecuteNonQuery();
+                    txtNombre.Text = "";
+                    txtAutor.Text = "";
+                    txtIsbn.Text = "";
+                    SqlCommand comand = new SqlCommand("SELECT * FROM libros;", conexionDB);
+                    SqlDataAdapter adaptador = new SqlDataAdapter();
+                    adaptador.SelectCommand = comand;
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+                    dataGridView1.DataSource = tabla;
+                }
+                else
+                {
+                    MessageBox.Show("Tienes que rellenar todos los campos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Solo puedes introducir campos numéricos en el ISBN", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            SqlCommand comand = new SqlCommand("SELECT * FROM libros;", conexionDB);
+            SqlDataAdapter adaptador = new SqlDataAdapter();
+            adaptador.SelectCommand = comand;
+            DataTable tabla = new DataTable();
+            adaptador.Fill(tabla);
+            dataGridView1.DataSource = tabla;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                DataGridViewRow fila = dataGridView1.Rows[e.RowIndex];
+                //int row = e.RowIndex;
+                string idBorrar = fila.Cells[2].Value.ToString();
+                SqlCommand comando = new SqlCommand("DELETE FROM libros WHERE id = '" + idBorrar + "'", conexionDB);
+                int r = comando.ExecuteNonQuery();
+                SqlCommand comand = new SqlCommand("SELECT * FROM libros;", conexionDB);
+                SqlDataAdapter adaptador = new SqlDataAdapter();
+                adaptador.SelectCommand = comand;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                dataGridView1.DataSource = tabla;
+            }
+            if (e.ColumnIndex == 1)
+            {
+                DataGridViewRow fila = dataGridView1.Rows[e.RowIndex];
+                string id = fila.Cells[2].Value.ToString();
+                string query = "SELECT nombre FROM libros WHERE id = '" + id + "'";
+                SqlCommand comando = new SqlCommand(query, conexionDB);
+                string nombre = comando.ExecuteScalar().ToString();
+                SqlCommand comand = new SqlCommand("SELECT autor FROM libros WHERE id = '" + id + "'", conexionDB);
+                string autor = comand.ExecuteScalar().ToString();
+                SqlCommand comando2 = new SqlCommand("SELECT isbn FROM libros WHERE id = '" + id + "'", conexionDB);
+                string isbn = comando2.ExecuteScalar().ToString();
+                idEditar = id;
+                txtNombre.Text = nombre;
+                txtAutor.Text = autor;
+                txtIsbn.Text = isbn;
+                cont += 1;
+            }
+            if (e.ColumnIndex == 2)
+            {
+                DataGridViewRow fila = dataGridView1.Rows[e.RowIndex];
+                string libroIsbn = fila.Cells[6].Value.ToString();
+                string fechaActual = DateTime.Now.ToString("f");
+                MessageBox.Show(fechaActual);
+
+                //DESHABILITAR LOS FORMULARIOS//
+                label1.Text = "Nombre Cliente";
+                txtIsbn.Enabled = false;
+                txtAutor.Enabled = false;
+                txtIsbn.Visible = false;
+                txtAutor.Visible = false;
+                btnAdd.Visible = false;
+                btnAdd.Enabled = false;
+                btnUpdate.Visible = false;
+                btnUpdate.Enabled = false;
+                btnRefresh.Visible = false;
+                btnRefresh.Enabled = false;
+                btnDelete.Visible = false;
+                btnDelete.Enabled = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                btnSend.Visible = true;
+                btnSend.Enabled = true;
+                ////////////////////////////////
+               
+                SqlCommand comando = new SqlCommand("INSERT INTO registros VALUES ('' ,'" + libroIsbn + "', '" + fechaActual + "', '" + fechaActual + "', 'Roberto' )");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            //HAY QUE COMPROBAR SI TIENE LETRAS DENTRO//
+            if (todoNumeros(txtIsbn.Text))
+            {
+                if (cont >= 1)
+                {
+                    if (txtNombre.Text != "" && txtAutor.Text != "" && txtIsbn.Text != "")
+                    {
+                        string query = "UPDATE libros SET nombre = '" + txtNombre.Text + "', autor = '" + txtAutor.Text + "', isbn = '" + txtIsbn.Text + "' WHERE id = '" + idEditar + "';";
+                        SqlCommand comando = new SqlCommand(query, conexionDB);
+                        int r = comando.ExecuteNonQuery();
+                        SqlCommand comand = new SqlCommand("SELECT * FROM libros;", conexionDB);
+                        SqlDataAdapter adaptador = new SqlDataAdapter();
+                        adaptador.SelectCommand = comand;
+                        DataTable tabla = new DataTable();
+                        adaptador.Fill(tabla);
+                        dataGridView1.DataSource = tabla;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tienes que rellenar todos los campos", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Tienes que hacer click en la tabla para seleccionar el campo", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Solo puedes introducir elementos numéricos en el campo ISBN", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+    }
+}
